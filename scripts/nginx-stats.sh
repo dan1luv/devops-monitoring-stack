f71588c58a1de0300c8eaf7c5c16d41d2cbb294a
Evgeny <dan1luv3d@gmail.com>
2026-07-20 15:09:37 +0000

    feat: добавил скрипт анализа nginx логов

--- scripts/nginx-stats.sh
+++ scripts/nginx-stats.sh
@@ -14,3 +14,7 @@ ERRORS=$(grep -c " 4[0-9][0-9] " $LOG)
 echo "Всего запросов: $TOTAL"
 echo "Успешных (200): $SUCCESS"
 echo "Ошибок (4хх): $ERRORS"
+
+echo ""
+echo "Топ IP адресов"
+awk '{print $1}' $LOG | sort | uniq -c | sort -rn | head -3
