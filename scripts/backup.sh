c9fa0c66ac274c410abc83a29e88958d6fad04ef
Evgeny <dan1luv3d@gmail.com>
2026-07-23 20:13:06 +0000

    тест

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,3 +7,6 @@ mkdir -p $BACKUP_DIR
 tar -czf $BACKUP_DIR/backup-$DATE.tar.gz $SOURCE 2>/dev/null
 find $BACKUP_DIR -name "*.tar.gz" -mtime +7 -delete
 echo "$(date) OK: бэкап создан backup-$DATE.tar.gz" >> ~/backup.log
+
+тестовая строчка
+
