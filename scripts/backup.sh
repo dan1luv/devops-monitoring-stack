463c65e9fedb6856b5392b7d843f869e56cdb6d8
Evgeny <dan1luv3d@gmail.com>
2026-07-23 20:14:30 +0000

    Revert "тест"
    This reverts commit c9fa0c66ac274c410abc83a29e88958d6fad04ef.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,6 +7,3 @@ mkdir -p $BACKUP_DIR
 tar -czf $BACKUP_DIR/backup-$DATE.tar.gz $SOURCE 2>/dev/null
 find $BACKUP_DIR -name "*.tar.gz" -mtime +7 -delete
 echo "$(date) OK: бэкап создан backup-$DATE.tar.gz" >> ~/backup.log
-
-тестовая строчка
-
